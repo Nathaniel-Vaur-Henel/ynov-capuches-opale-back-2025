b78fd664232b9ee481a6abf7bf250b22134bed03
Nathaniel Vaur Henel <nathaniel@vaurhenel.it>
2025-02-06 23:35:59 +0100

    Ajoute de la CI au push sur les branches

---
+++ src/test/java/com/ynov/capuches/opale/TestTest.java
@@ -0,0 +1,18 @@
+package com.ynov.capuches.opale;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestTest {
+
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void fail() {
+        assertEquals(1, 2);
+    }
+}
